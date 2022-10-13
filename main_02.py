9957498930e54e7a61692cdaf77e9c3e8c901803
JosteinRuen <99617155+JosteinRuen@users.noreply.github.com>
2022-10-13 13:02:24 +0200

    Modified csv saving

--- main_02.py
+++ main_02.py
@@ -5,10 +5,10 @@ import json
 from pyjstat import pyjstat
 
 
-def kombiner_sporringer(df_1, df_2): #Formaterer og kombinerer 2 dataframes
+def kombiner_sporringer(df_1, df_2):  # Formaterer og kombinerer 2 dataframes
     print(df_1.describe)
 
-    #Først rename alle ringerike(1977) til bare Ringerike etc
+    # Først rename alle ringerike(1977) til bare Ringerike etc
     for row_label, row in df_1.iterrows():
         if row['region'] == 'Ringerike (1977-2019)':
             df_1.loc[row_label, 'region'] = 'Ringerike'
@@ -19,25 +19,23 @@ def kombiner_sporringer(df_1, df_2): #Formaterer og kombinerer 2 dataframes
         if row['region'] == 'Modum (-2019)':
             df_1.loc[row_label, 'region'] = 'Modum'
 
-
     dataframes = [df_1, df_2]
-    sammensatt_frames = pd.concat(dataframes) #Concat dataframes til 1 dataframe
+    sammensatt_frames = pd.concat(dataframes)  # Concat dataframes til 1 dataframe
 
     return sammensatt_frames
 
 
+def dataframe_to_linechart(df):  # Formaterer data til highchart formatet(år, Ringerike, Hole, Modum, Jevnaker)
 
-def dataframe_to_linechart(df):  # Formaterer data til highchart formatet(år, Ringerike_verdi, Hole_verdi, Modum_verdi, Jevnaker_verdi)
-
-    #Lag tomme lister med nettinnflytning som data
+    # Lag tomme lister med nettinnflytning som data
     ringerike_list = []
     hole_list = []
     modum_list = []
     jevnaker_list = []
 
-    #Iterer over dataframe
+    # Iterer over dataframe
     for row_label, row in df.iterrows():
-        if row['region'] == 'Ringerike': #Hvis regionen er ringerike...
+        if row['region'] == 'Ringerike':  # Hvis regionen er ringerike...
             # Legg til nettofinnflytnings verdien i listen
             ringerike_list.append([row['år'], row['value'], row['region']])
         if row['region'] == 'Hole':
@@ -47,24 +45,23 @@ def dataframe_to_linechart(df):  # Formaterer data til highchart formatet(år, R
         if row['region'] == 'Jevnaker':
             jevnaker_list.append([row['år'], row['value'], row['region']])
 
-
     # Resultat_string bygger opp CSV filen som highcharts skal lese av
     resultat_string = "År, Ringerike, Hole, Modum, Jevnaker\n"
 
     # Iterer over alle elementer i ringerike listen
     for i in ringerike_list:
-        resultat_string += i[0] + ', ' #Legger til året i csv filen
-        resultat_string += str(i[1]) + ', ' #legger til ringerike sin netto-verdi
+        resultat_string += i[0] + ', '  # Legger til året i csv filen
+        resultat_string += str(i[1]) + ', '  # legger til ringerike sin netto-verdi
 
         # for hvert element i listen, iterer de andre listene og slå sammen til 1 CSV fil
         for x in hole_list:
-            if(x[0] == i[0]): # Hvis ringerike-år er det samme som hole-år...
-                resultat_string += str(x[1]) + ', ' # Legg til hole nettoverdi i CSV filen
+            if (x[0] == i[0]):  # Hvis ringerike-år er det samme som hole-år...
+                resultat_string += str(x[1]) + ', '  # Legg til hole nettoverdi i CSV filen
                 break
 
         for x in modum_list:
-            if(x[0] == i[0]):
-                resultat_string+= str(x[1]) + ', '
+            if (x[0] == i[0]):
+                resultat_string += str(x[1]) + ', '
                 break
 
         for x in jevnaker_list:
@@ -72,26 +69,18 @@ def dataframe_to_linechart(df):  # Formaterer data til highchart formatet(år, R
                 resultat_string += str(x[1])
                 break
 
-        resultat_string+= '\n'
+        resultat_string += '\n'
 
     with open('verdier.csv', 'w', encoding='UTF8') as f:
         skriver = csv.writer(f)
         f.write(resultat_string)
 
-
-    # for col_label, col in df.items():
-    #   print(col_label, col, sep='\n')
-    #  if (col_label == 'år'):
-    #     print()
-
-
-    # print(newdf.describe)
     return df
 
 
 def main():
     url = "https://data.ssb.no/api/v0/no/table/09588/"
-    sporring_77 = {
+    sporring_netto_77 = {
         "query": [
             {
                 "code": "Region",
@@ -143,7 +132,7 @@ def main():
             "format": "json-stat2"
         }
     }
-    sporring_20 = {
+    sporring_netto_20 = {
         "query": [
             {
                 "code": "Region",
@@ -181,26 +170,126 @@ def main():
             "format": "json-stat2"
         }
     }
+    sporring_inn_ut_77 = {
+        "query": [
+            {
+                "code": "Region",
+                "selection": {
+                    "filter": "agg_single:KommGjeldende",
+                    "values": [
+                        "0532",
+                        "0605",
+                        "0612",
+                        "0623"
+                    ]
+                }
+            },
+            {
+                "code": "ContentsCode",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "Innflytting",
+                        "Utflytting"
+                    ]
+                }
+            },
+            {
+                "code": "Tid",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2004",
+                        "2005",
+                        "2006",
+                        "2007",
+                        "2008",
+                        "2009",
+                        "2010",
+                        "2011",
+                        "2012",
+                        "2013",
+                        "2014",
+                        "2015",
+                        "2016",
+                        "2017",
+                        "2018",
+                        "2019"
+                    ]
+                }
+            }
+        ],
+        "response": {
+            "format": "json-stat2"
+        }
+    }
+    sporring_inn_ut_20 = {
+        "query": [
+            {
+                "code": "Region",
+                "selection": {
+                    "filter": "agg_single:Komm2020",
+                    "values": [
+                        "3007",
+                        "3038",
+                        "3047",
+                        "3053"
+                    ]
+                }
+            },
+            {
+                "code": "ContentsCode",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "Innflytting",
+                        "Utflytting"
+                    ]
+                }
+            },
+            {
+                "code": "Tid",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2020",
+                        "2021"
+                    ]
+                }
+            }
+        ],
+        "response": {
+            "format": "json-stat2"
+        }
+    }
 
-    x = requests.post(url, json=sporring_77) #save response in x
+    x = requests.post(url, json=sporring_netto_77)  # save response in x
     response_77 = json.loads(x.text)
 
-    dataset = pyjstat.Dataset.read(x.text) # Create  dataset out of the response
-    df_77 = dataset.write('dataframe') # Create a pandas dataframe
+    dataset = pyjstat.Dataset.read(x.text)  # Create  dataset out of the response
+    df_netto_77 = dataset.write('dataframe')  # Create a pandas dataframe
 
-    x = requests.post(url, json=sporring_20)
+    x = requests.post(url, json=sporring_netto_20)
     response_20 = json.loads(x.text)
 
     dataset = pyjstat.Dataset.read(x.text)
-    df_20 = dataset.write('dataframe')
+    df_netto_20 = dataset.write('dataframe')
 
-    df_kombinert = kombiner_sporringer(df_77, df_20)
+    x = requests.post(url, json=sporring_inn_ut_77)
+    dataset = pyjstat.Dataset.read(x.text)
+    df_inn_ut_77 = dataset.write('dataframe')
 
-    print(df_kombinert.describe)
+    x = requests.post(url, json=sporring_inn_ut_20)
+    dataset = pyjstat.Dataset.read(x.text)
+    df_inn_ut_20 = dataset.write('dataframe')
+
+    df_kombinert = kombiner_sporringer(df_netto_77, df_netto_20)
+
+    df_inn_ut_kombinert = kombiner_sporringer(df_inn_ut_77, df_inn_ut_20)
+
+    print(df_inn_ut_kombinert.describe)
+
     newdf = dataframe_to_linechart(df_kombinert)
 
+
 main()
-
-
-
-
